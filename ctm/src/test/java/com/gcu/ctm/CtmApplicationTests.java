e1ba99445412db9cbc105f854e28175787b4ec2c
Nolan Tuttle <NTuttle1@my.gcu.edu>
2025-03-26 14:41:43 -0700

    Initialize Spring project

---
+++ ctm/src/test/java/com/gcu/ctm/CtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcu.ctm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
